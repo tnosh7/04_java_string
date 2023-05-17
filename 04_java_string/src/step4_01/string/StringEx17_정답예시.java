7642433ebbc73f66b0133ba10f45c0a018cb6154
user <user@LAPTOP-S5PHO2U3>
2023-05-17 11:26:23 +0900

    23-05-17

--- 04_java_string/src/step4_01/string/StringEx17_정답예시.java
+++ 04_java_string/src/step4_01/string/StringEx17_정답예시.java
@@ -27,27 +27,27 @@ public class StringEx17_정답예시 {
 		System.out.print("변경하고 싶은 단어를 입력하세요 : ");
 		String word = scan.nextLine();
 		
-		char[] arr = new char[text.length()];
-		for(int i = 0; i < text.length(); i++) {
-			arr[i] = text.charAt(i);
+		char[] arr = new char[text.length()];		//for문 밖에서 배열 만들기 
+		for(int i = 0; i < text.length(); i++) {		
+			arr[i] = text.charAt(i);				//한글자씩 넣어줌 
 		}
 		
-		int wordSize = word.length();
-		int idx = 0;
-		boolean isSame = false;
+		int wordSize = word.length();				//변경할 단어 길이 
+		int idx = 0;								//단어 인덱스 체크용
+		boolean isSame = false;						//true면 변경할 단어 적용하기 
 		
-		for(int i = 0; i < arr.length - wordSize + 1; i++) {
+		for(int i = 0; i < arr.length - wordSize + 1; i++) {	//원래 단어 길이에서 변경할 단어 길이 빼줌 + 1
 			
-			int correctCnt = 0;
+			int correctCnt = 0;								//단어 셀 용도
 			
-			for(int j = 0; j < wordSize; j++) {
-				if(arr[i+j] == word.charAt(j)) {
-					correctCnt++;
+			for(int j = 0; j < wordSize; j++) {				//
+				if(arr[i+j] == word.charAt(j)) {			//arr길이와 단어 길이가 같으면 
+					correctCnt++;							//알파벳 cnt 
 				}
 			}
 			
-			if(correctCnt == wordSize) {
-				idx = i;
+			if(correctCnt == wordSize) {					//cnt 와일치하면 
+				idx = i;									//인덱스 뽑기 
 				isSame = true;
 			}
 			
@@ -56,24 +56,24 @@ public class StringEx17_정답예시 {
 		
 		if (isSame) {
 			
-			int firstIdx = idx;
-			int lastIdx = idx + wordSize;
+			int firstIdx = idx;							//첫 알파벳
+			int lastIdx = idx + wordSize;				//마지막 알파벳 
 			
-			System.out.print("무엇으로 바꾸시겠습니까?");
+			System.out.print("무엇으로 바꾸시겠습니까?");		
 			String change = scan.nextLine();
 			
-			char[] temp = arr;
-			String front = "";
-			for (int i = 0; i < firstIdx; i++) {
+			char[] temp = arr;							//temp 에 단어 옮겨줌 
+			String front = "";					
+			for (int i = 0; i < firstIdx; i++) {		//첫 알파벳 앞까지 더해줌  	
 				front += temp[i];
 			}
 			
 			String back = "";
-			for (int i = lastIdx; i < temp.length; i++) {
+			for (int i = lastIdx; i < temp.length; i++) {	//마지막 인덱스 
 				back += temp[i];
 			}
 			
-			String result = front + change + back;
+			String result = front + change + back;		
 			System.out.println(result);
 			
 		}
--- 04_java_string/src/step4_01/string/StringEx17_풀이.java
+++ 04_java_string/src/step4_01/string/StringEx17_풀이.java
@@ -28,7 +28,52 @@ public class StringEx17_풀이 {
 		System.out.print("변경하고 싶은 단어를 입력하세요 : ");
 		String word = scan.nextLine();
 		
+		//단어 길이와 알파벳 하나씩 비교 
+		char[]arr = new char[text.length()];	//for문안이 아닌 밖에서 생성.
+		int tleng = word.length();
+		int idx = 0;
+		boolean isSame= false;
 		
+		
+		for (int i = 0; i < text.length(); i++) {
+			arr[i] = text.charAt(i);
+		}
+		
+		for (int i = 0; i < arr.length - tleng + 1; i++) {
+			int cnt =0;
+			for (int j = 0; j < tleng; j++) {
+				if(arr[i+j]==word.charAt(j)) {
+					cnt++;
+				}
+			}
+			if (cnt == tleng) {
+				idx =i;
+				isSame = true;
+			}
+		}
+		
+		
+		if(isSame) {
+			int firstIdx =idx;
+			int lastIdx =idx +tleng;
+			
+			System.out.println("무엇으로 바꾸시겠습니까");
+			String change = scan.next();
+			
+			char[]temp = arr;
+			String front ="";
+			for (int i = 0; i < firstIdx; i++) {
+				front +=temp[i];
+			}
+			String back = "";
+			for (int i = lastIdx; i < temp.length; i++) {
+				back +=temp[i];
+			}
+			String result = front+change + back;
+			System.out.println(result);
+			
+		}
+		else System.out.println("해당 단어는 존재하지 않습니다.");
 		
 		
 	}
